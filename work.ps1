6041c177d64498495e7e02e1f3740bcaceae3b0f
johnwilliams199032 <johnwilliams199032@gmail.com>
2019-07-26 01:21:15 -0600

    initial commit

---
+++ work.ps1
@@ -0,0 +1,69 @@
+﻿
+# foreach ($i in get-process) {
+#   Write-Host $i
+# }
+# Get-Process | sort name 
+
+
+function puts ($a){write-host $a}
+
+# function Printit ($a){
+#   foreach ($i in 0..$a.length){
+#     puts $a[$i]
+#   }
+# }
+
+function Printit ($a){  
+  if ($b -eq $a.length){ return }
+  elseif ($b -eq $null) {
+    puts $a[0]
+  }
+  else {
+    puts $a[$b]
+  }
+  $b = $b + 1 
+  printit ($a)
+}
+
+
+function count($a){
+  if ($b -eq $a -or $a -eq 0){return $a}
+  if ($b -eq $null) {
+  $b = 1
+  }
+  $b = $b +1
+  puts $b
+  count($a)
+}
+
+# factorial function
+function factorial($number){
+  if (1 -eq $number){return 1}
+  if ($b -eq $null) {
+    $b = $number 
+    
+   $b = $b * $(factorial($($number - 1 )))
+   # puts $b
+   return $b
+  }
+  else {
+  # puts $b ;
+   return $number * $(factorial($number-1))}
+}
+
+
+function max () {
+  foreach( $i in 1..200) {
+    $x = factorial($i)
+    if ($x -eq [double]::PositiveInfinity){
+      puts "Infinity reached at $i! = $X"
+      puts "$($i-1)! = " 
+      $(factorial($i -1))
+      break
+    }
+  }
+}
+max
+ # max
+ # printit "asdasdasdasd"
+ # count 10000000
